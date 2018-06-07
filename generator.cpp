c7747b85ff94e0c3519ad135359b64674c04c640
Yousef-Zoumot <yzoumot17@gmail.com>
2018-06-07 11:28:50 -0700

    adding divide and remainder

--- generator.cpp
+++ generator.cpp
@@ -313,8 +313,6 @@ void Remainder::generate()
 
 void Negate::generate()
 {
-  // _expr->generate();
-  //
   // cout << "\tmovl\t" << _expr << ", %eax" << endl;
   // cout << "\tnegl\t" << "%eax" << endl;
   // cout << "\taddl\t" << ? << endl;
@@ -334,9 +332,6 @@ void Negate::generate()
 
 void Not::generate()
 {
-  // _expr->generate();
-  //
-  //
   // cout << "\tmovl\t" << _expr << ", %eax" << endl;
   // cout << "\tcmpl\t" << "$0, %eax" << endl;
   // cout << "\tsete %al" << endl;
@@ -357,8 +352,6 @@ void Not::generate()
 
 void Dereference::generate()
 {
-  // _expr->generate();
-  //
   // cout <<"\tmovl\t" << _expr << ", %rax" << endl;
   // cout << (_type.size() == 1 ? "\tmovsbl\t" : "\tmovl\t") << "\t(%rax), %rax" << endl;
   // cout << "\tmovl\t" << "%rax, " << ? << endl;
@@ -387,29 +380,29 @@ void Address::generate()
   assign(this, _expr->_register);
 }
 
-// void LogicalAnd::generate()
-// {
-//   _left->generate();
-//   _right->generate();
-//
-//   Label label;
-//   stringstream ss;
-//   ss << label;
-//
-//   cout << "\tmov\t" << _left << ", %rax" << endl;
-//   cout << "\tcmp\t" << "$0, %rax" << endl;
-//   cout << "je\t" << label << ":" << endl;
-//
-//   cout << "\tmov\t" << _right << ", %rax" << endl;
-//   cout << "\tcmp\t" << "$0, %rax" << endl;
-//   cout << label << ":" << endl;
-//   cout << "\tsetne\t" << "%al" << endl;
-//   cout << "\tmovzbl\t" << "%al, %eax" << endl;
-//   cout << "\tmov\t" << "%eax, " << ? << endl;
-//
-// }
-//
-//
+void LogicalAnd::generate()
+{
+  _left->generate();
+  _right->generate();
+
+  Label label;
+  stringstream ss;
+  ss << label;
+
+  cout << "\tmov\t" << _left << ", %rax" << endl;
+  cout << "\tcmp\t" << "$0, %rax" << endl;
+  cout << "je\t" << label << ":" << endl;
+
+  cout << "\tmov\t" << _right << ", %rax" << endl;
+  cout << "\tcmp\t" << "$0, %rax" << endl;
+  cout << label << ":" << endl;
+  cout << "\tsetne\t" << "%al" << endl;
+  cout << "\tmovzbl\t" << "%al, %eax" << endl;
+  cout << "\tmov\t" << "%eax, " << ? << endl;
+
+}
+
+
 
 void While::generate()
 {
